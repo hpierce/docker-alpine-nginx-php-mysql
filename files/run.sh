7b360c4526741ad36b437984486465524415749e
Hugh Pierce <hpierce3213@gmail.com>
2016-10-10 19:07:04 -0400

    upgraded php and ending in bash

--- files/run.sh
+++ files/run.sh
@@ -1,27 +1,5 @@
-#!/bin/sh
-
-[ -f /run-pre.sh ] && /run-pre.sh
-
-if [ ! -d /data/htdocs ] ; then
-  mkdir -p /data/htdocs
-  chown :www-data /data/htdocs
-fi
-
-# create all mysql neccessary database
-if [ ! -f /var/lib/mysql/ibdata1 ]; then
-  mysql_install_db
-fi
-
-# start mysql
-mysqld --skip-grant-tables &
-
-# start php-fpm
-mkdir -p /data/logs/php-fpm
-php-fpm
-
-# start nginx
-mkdir -p /data/logs/nginx
-mkdir -p /data/logs/php-fpm
-mkdir -p /tmp/nginx
-chown nginx /tmp/nginx
-nginx
+#!/bin/bash
+/usr/bin/mysqld_safe --datadir="/var/lib/mysql" >/dev/null &
+php-fpm7 &
+nginx &
+/bin/bash
